3bbaf5f79dcf273bc9754da3d7c7b82f344204e0
Leshnevskyi <leshnevskyi@gmail.com>
2022-09-27 03:06:07 +0300

    feat: extended complex number methods

--- source/packages/math/Complex.ts
+++ source/packages/math/Complex.ts
@@ -1,18 +1,45 @@
+import {isNumber} from 'lodash';
+
 class Complex {
 	constructor(public re: number, public im: number) {}
 
-	static from(other: Complex) {
-		return new Complex(other.re, other.im);
-	} 
+	static from(other: Complex | number) {
+		return isNumber(other) 
+			? new Complex(other, 0) 
+			: new Complex(other.re, other.im);
+	}
 
-	add(other: Complex) {
+	add(other: Complex | number) {
+		other = Complex.from(other);
+
 		return new Complex(this.re + other.re, this.im + other.im);
 	}
 
-	multiply(other: Complex) {
+	subtract(other: Complex | number) {
+		other = Complex.from(other);
+
+		return this.add(other.additiveInverse);
+	}
+
+	multiply(other: Complex | number) {
+		other = Complex.from(other);
+		
 		return new Complex(
 			this.re * other.re - this.im * other.im, 
 			this.re * other.im + this.im * other.re
+		);
+	}
+
+	divide(other: Complex | number) {
+		if (isNumber(other)) {
+			return new Complex(this.re / other, this.im / other);
+		}
+
+		const commonDenominator = other.re ** 2 + other.im ** 2;
+
+		return new Complex(
+			(this.re * other.re + this.im * other.im) / commonDenominator,
+			(this.im * other.re - this.re * other.im) / commonDenominator
 		);
 	}
 
@@ -22,8 +49,12 @@ class Complex {
 			: this.multiply(this.pow(exponent - 1));
 	}
 
+	get additiveInverse() {
+		return new Complex(-this.re, -this.im);
+	}
+
 	get abs() {
-		return Math.sqrt(Math.pow(this.re, 2) + Math.pow(this.im, 2));
+		return Math.sqrt(this.re ** 2 + this.im ** 2);
 	}
 }
 
