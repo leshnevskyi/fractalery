e64b8fdfa3f789033b79e9c25da8cb3572f49f76
Leshnevskyi <leshnevskyi@gmail.com>
2022-11-24 04:45:28 +0200

    refactor: move icons to separate files

--- source/components/Alert/index.tsx
+++ source/components/Alert/index.tsx
@@ -4,6 +4,10 @@ import {AnimatePresence, motion} from 'framer-motion';
 
 import {modalContainerElement} from 'containerElements';
 
+import {ReactComponent as SuccessIcon} from 'assets/checkmark.svg';
+import {ReactComponent as WarningIcon} from 'assets/warning.svg';
+import {ReactComponent as ErrorIcon} from 'assets/error.svg';
+
 interface Props {
 	isDisplayed: boolean,
 	closingTimeout?: number,
@@ -11,62 +15,6 @@ interface Props {
 	onClose?: () => void,
 	type: 'success' | 'warning' | 'error',
 }
-
-const WarningIcon = () => {
-	return (
-		<svg 
-			fill='none' 
-			viewBox='0 0 24 24' 
-			strokeWidth='1.5' 
-			stroke='currentColor'
-			className='stroke-orange' 
-		>
-			<path 
-				strokeLinecap='round'
-				strokeLinejoin='round' 
-				d='
-					M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 
-					0118 0zm-9 3.75h.008v.008H12v-.008z
-				'
-			/>
-		</svg>
-	);
-};
-
-const SuccessIcon = () => {
-	return (
-		<svg fill='none'
-			viewBox='0 0 24 24'
-			strokeWidth='1.5'
-			stroke='currentColor'
-			className='stroke-green-500'
-		>
-			<path 
-				strokeLinecap='round'
-				strokeLinejoin='round'
-				d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
-			/>
-		</svg>
-	);
-};
-
-const ErrorIcon = () => {
-	return (
-		<svg 
-			fill='none' 
-			viewBox='0 0 24 24' 
-			strokeWidth='1.5' 
-			stroke='currentColor' 
-			className='stroke-red'
-		>
-			<path 
-				strokeLinecap='round' 
-				strokeLinejoin='round' 
-				d='M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z'
-			/>
-		</svg>
-	);
-};
 
 const renderedIcons = {
 	success: <SuccessIcon/>,
